32e3b171c20addb2d3ae3bfd486873163c46e4c9
Sean Kennedy <snkennedy21@gmail.com>
2022-10-04 18:02:41 -0400

    set up migrations file for decks. Add __init__.py and __main__.py file. Added a CREATE TABLE SQL command for cards

---
+++ decks/migrations/001_create_my_table.py
@@ -0,0 +1,14 @@
+steps = [
+  [
+    '''
+    CREATE TABLE cards (
+      id SERIAL PRIMARY KEY NOT NULL,
+      multiverse_id INTEGER NOT NULL,
+      name VARCHAR(1000) NOT NULL
+    );
+    ''',
+    '''
+    DROP TABLE vacations
+    '''
+  ]
+]
