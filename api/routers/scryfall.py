52ca4223345ab8a5a779ad00af8380646fc34db4
Devin Wright <dtw2111@gmail.com>
2022-10-11 03:35:18 -0400

    refactor basic search to account for paginated results

--- api/routers/scryfall.py
+++ api/routers/scryfall.py
@@ -9,6 +9,7 @@ router = APIRouter(tags=["scryfall"])
 async def search_scryfall(
     search: str
 ):
+    output = {"cards": []}
     url = f"https://api.scryfall.com/cards/search?q={search}"
     response = requests.get(url)
     content = json.loads(response.content)
@@ -16,31 +17,39 @@ async def search_scryfall(
     if content.get("code") == "not_found" or content.get("total_cards") == 0:
         return error_msg
 
-    output = {"cards": []}
-    cards = content.get("data")
-    for card in cards:
-        if len(card.get("multiverse_ids")) == 0 or card.get("layout") == "art_series":
-            continue # we don't care about tokens, alchemy cards, art series
+    def collect(cards): # appends cards in input list to output["cards"]
+        for card in cards:
+            if len(card.get("multiverse_ids")) == 0 or card.get("layout") == "art_series":
+                continue # we don't care about tokens, alchemy cards, art series, anything without a multiverse id
 
-        if card.get("layout") in [
-            "modal_dfc", 
-            "transform",
-            ]: # double-faced card
-            object = {
-                "name": card.get("name"), # "front name // back name"
-                "multiverse_id": card.get("multiverse_ids")[0],
-                "picture_url": card.get("card_faces")[0].get("image_uris").get("normal"),
-                "back": {
-                    "picture_url": card.get("card_faces")[1].get("image_uris").get("normal"),
+            if card.get("layout") in [
+                "modal_dfc", 
+                "transform",
+                ]: # double-faced card types
+                object = {
+                    "name": card.get("name"), # "front name // back name"
+                    "multiverse_id": card.get("multiverse_ids")[0],
+                    "picture_url": card.get("card_faces")[0].get("image_uris").get("normal"),
+                    "back": {
+                        "picture_url": card.get("card_faces")[1].get("image_uris").get("normal"),
+                    }
                 }
-            }
-        else: # single-faced card
-            object = {
-                "name": card.get("name"),
-                "multiverse_id": card.get("multiverse_ids")[0],
-                "picture_url": card.get("image_uris").get("normal"),
-            }
+            else: # single-faced card
+                object = {
+                    "name": card.get("name"),
+                    "multiverse_id": card.get("multiverse_ids")[0],
+                    "picture_url": card.get("image_uris").get("normal"),
+                }
 
-        output["cards"].append(object)
+            output["cards"].append(object)
+    
+    collect(content["data"])
+
+    while content["has_more"]: # collect cards from rest of paginated results
+        url = content["next_page"]
+        response = requests.get(url)
+        content = json.loads(response.content)
+        collect(content["data"])
+
 
     return error_msg if len(output["cards"]) == 0 else output
