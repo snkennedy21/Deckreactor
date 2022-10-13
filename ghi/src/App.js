57cbe480175b1e53e72356e161bd56c779c7518f
Sean Kennedy <snkennedy21@gmail.com>
2022-10-13 19:11:32 -0400

    set up navbar and basic query to scryfall api

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,37 +1,17 @@
-import { useEffect, useState } from 'react';
-import Construct from './Construct.js'
-import ErrorNotification from './ErrorNotification';
-import './App.css';
+import { useEffect, useState } from "react";
+import Navbar from "./components/ui/Navbar";
+import SearchResults from "./components/search/SearchResults";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);  
-
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
-      console.log('fastapi url: ', url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
-
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, [])
-
-
   return (
-    <div>
-      <ErrorNotification error={error} />
-      <Construct info={launch_info} />
-    </div>
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="search" element={<SearchResults />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ ghi/src/components/ui/Navbar.js
@@ -0,0 +1,78 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { useState } from "react";
+
+function NavScrollExample() {
+  const [listOfSearchCards, setListOfSearchCards] = useState([]);
+  const [search, setSearch] = useState("");
+
+  function updateSearchTermHandler(e) {
+    setSearch(e.target.value);
+  }
+
+  function queryScryfallHandler(e) {
+    e.preventDefault();
+    fetchCardDataFromScryfall(search);
+  }
+
+  async function fetchCardDataFromScryfall(search) {
+    const scryfallUrl = `http://localhost:8000/scryfall/${search}`;
+    const scryfallResponse = await fetch(scryfallUrl);
+
+    if (scryfallResponse.ok) {
+      const scryfallData = await scryfallResponse.json();
+      console.log(scryfallData);
+    }
+  }
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form onSubmit={queryScryfallHandler} className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+              onChange={updateSearchTermHandler}
+              value={search}
+            />
+            <Button type="submit" variant="outline-success">
+              Search
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavScrollExample;
