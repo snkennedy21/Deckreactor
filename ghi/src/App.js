9ae9a17f04c5eeaf752d78a40a14307bcb0d1537
thomas brand <tbrand61@gmail.com>
2022-10-18 13:43:37 -0400

    added Footer to components and added style sheet to App,js in src

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -6,21 +6,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import CardDetailPage from "./components/card-details/CardDetailPage";
 import MyCollection from "./components/collection/CollectionList";
 import MyCollection2 from "./components/collection/CollectionCards";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
 
 function App() {
-  const domain = /https:\/\/[^/]+/
-  const basename = process.env.PUBLIC_URL.replace(domain, '');
+  const domain = /https:\/\/[^/]+/;
+  const basename = process.env.PUBLIC_URL.replace(domain, "");
 
   return (
-    <BrowserRouter basename={basename}>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="search" element={<SearchResults />}/>
-        <Route path="card/:multiverse_id" element={<CardDetailPage />}/>
-        <Route path="edit_collection" element={<MyCollection />}></Route>   
-        <Route path="collection" element={<MyCollection2 />}></Route>  
-      </Routes>
-    </BrowserRouter>
+    <div className="page-container">
+      <div className="content-wrap">
+        <BrowserRouter basename={basename}>
+          <Navbar />
+          <Routes>
+            <Route path="search" element={<SearchResults />} />
+            <Route path="card/:multiverse_id" element={<CardDetailPage />} />
+            <Route path="edit_collection" element={<MyCollection />}></Route>
+            <Route path="collection" element={<MyCollection2 />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
--- ghi/src/components/collection/CollectionList.js
+++ ghi/src/components/collection/CollectionList.js
@@ -1,138 +1,218 @@
-import Table from 'react-bootstrap/Table';
-import { useEffect, useState, useReducer } from 'react';
-import Button from 'react-bootstrap/Button';
-import {Link} from 'react-router-dom';
-import './collection.css'
-
+import Table from "react-bootstrap/Table";
+import { useEffect, useState, useReducer } from "react";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import "./collection.css";
 
 // const my_collection = "knight"
-const account_id = "6349a18d7b649afdd348ae7f"
+const account_id = "634ed235c46bd0e65d85bdae";
 
 export default function MyCollection() {
   const [collection, setCollection] = useState([]);
-  const [filteredCollection, setFilteredCollection] = useState([])
+  const [filteredCollection, setFilteredCollection] = useState([]);
 
   // as reducer changes from card removal, useEffect fetches card collection
-  const [reducerValue, collectionUpdater] = useReducer(x => x + 1, 0);
-
+  const [reducerValue, collectionUpdater] = useReducer((x) => x + 1, 0);
 
   const handleDelete = (multiverse_id) => {
-      fetch(`http://localhost:8000/collections/${account_id}/remove_one/${multiverse_id}`, { 
-      method: "PUT", 
-      headers: { 
-        accept: 'application/json'
-     }
-    })
-      collectionUpdater()
-  }
+    fetch(
+      `http://localhost:8000/collections/${account_id}/remove_one/${multiverse_id}`,
+      {
+        method: "PUT",
+        headers: {
+          accept: "application/json",
+        },
+      }
+    );
+    collectionUpdater();
+  };
 
   // ###################################### handle delete and refetch to update state on ui
 
   useEffect(() => {
     async function getCollection() {
       // const url = `http://localhost:8000/scryfall/${my_collection}`
-      const url = `http://localhost:8000/collections/${account_id}`
+      const url = `http://localhost:8000/collections/${account_id}`;
       const response = await fetch(url);
       if (response.ok) {
-        const data = await response.json()
-        console.log(`${data.cards.length} card(s) rendered`)
-        setCollection(data.cards)
-        setFilteredCollection(data.cards)
+        const data = await response.json();
+        console.log(`${data.cards.length} card(s) rendered`);
+        setCollection(data.cards);
+        setFilteredCollection(data.cards);
       }
     }
     getCollection();
-    
+
     // whenever reducer value changes useEffect runs get collection
-  },[reducerValue])
-  
+  }, [reducerValue]);
+
   const handleInputChange = (event) => {
     let search = event.target.value.toLowerCase();
     let cardMatches = [];
-    collection.forEach(card => {
-        if (card.name.toLowerCase().includes(search)) {
-            cardMatches.push(card);
-        }
+    collection.forEach((card) => {
+      if (card.name.toLowerCase().includes(search)) {
+        cardMatches.push(card);
+      }
     });
-    setFilteredCollection(cardMatches)
-  }
+    setFilteredCollection(cardMatches);
+  };
 
-  let collection_value = 0
-  let collection_count = 0
+  let collection_value = 0;
+  let collection_count = 0;
 
-  for (let card of collection){
-    collection_count += Number(card["quantity"])
-    if (card["quantity"] >= 1 && card["card_price"] !== null){
-      collection_value += Number(card["quantity"]) * Number(card["card_price"]) 
+  for (let card of collection) {
+    collection_count += Number(card["quantity"]);
+    if (card["quantity"] >= 1 && card["card_price"] !== null) {
+      collection_value += Number(card["quantity"]) * Number(card["card_price"]);
     }
   }
 
-  if (filteredCollection.length === 0){
-    return(
-        <div className='container-fluid'>
-            <form onSubmit={event => {event.preventDefault()}} id="search-collection-form">
-                <div className="input-group mb-3 p-4">
-                    <input onChange={handleInputChange} className="form-control" name="searchCOLLECTION" id="searchCOLLECTION" type="search" placeholder="Search collection"/>
-                </div>
-            </form>
-            <div className="container">
-                <h1>Collection List</h1>
-                <p>No cards found from your search</p>
-            </div>
+  if (filteredCollection.length === 0) {
+    return (
+      <div className="container-fluid">
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+          id="search-collection-form"
+        >
+          <div className="input-group mb-3 p-4">
+            <input
+              onChange={handleInputChange}
+              className="form-control"
+              name="searchCOLLECTION"
+              id="searchCOLLECTION"
+              type="search"
+              placeholder="Search collection"
+            />
+          </div>
+        </form>
+        <div className="container">
+          <h1>Collection List</h1>
+          <p>No cards found from your search</p>
         </div>
-          )
-    }else{
-      return (
-        <div className="container" >
-            <form onSubmit={event => {event.preventDefault()}} id="search-collection-form">
-                <div className="input-group mb-3 p-4">
-                  <input onChange={handleInputChange} className="form-control" name="searchCOLLECTION" id="searchCOLLECTION" type="search" placeholder="Search collection"/>
-                </div>
-                <p><b>{filteredCollection.length} </b>cards found</p>
-            </form>
-            <div>
-                <b>Collection Value: <b className="text-success">${collection_value.toFixed(2)}</b></b>
-                <p>Total cards in collection: <b>{collection_count}</b></p>
-            </div>
-              <div className="table-wrapper-scroll-y my-custom-scrollbar">
-                <Table striped bordered hover variant="light" className="table table-bordered table-striped mb-0" >
-                  <thead>
-                      <tr>
-                          <th className="text-center col col-lg-2">Card Name</th>
-                          <th className="text-center col col-lg-2">Card Image</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                    { filteredCollection.map(row => {
-                      if (row.back_picture_url){
-              return (
-                <tr key ={row.multiverse_id}>
-                    <td className="text-center">{row.name} <br></br><p className="mt-3">Quantity: {row.quantity}</p>
-                      <Button variant="danger" size="xs" onClick={() => handleDelete(row.multiverse_id)}>Remove one card</Button><br></br><p className="mt-3">Card Price: {row.card_price}</p></td>
-                    <td className="text-center"><img className="p-1" src={row.picture_url} alt="card_picture" width="170px"/>
-                        <img className="p-1" src={row.back_picture_url} alt="card_picture" width="170px"/>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+          id="search-collection-form"
+        >
+          <div className="input-group mb-3 p-4">
+            <input
+              onChange={handleInputChange}
+              className="form-control"
+              name="searchCOLLECTION"
+              id="searchCOLLECTION"
+              type="search"
+              placeholder="Search collection"
+            />
+          </div>
+          <p>
+            <b>{filteredCollection.length} </b>cards found
+          </p>
+        </form>
+        <div>
+          <b>
+            Collection Value:{" "}
+            <b className="text-success">${collection_value.toFixed(2)}</b>
+          </b>
+          <p>
+            Total cards in collection: <b>{collection_count}</b>
+          </p>
+        </div>
+        <div className="table-wrapper-scroll-y my-custom-scrollbar">
+          <Table
+            striped
+            bordered
+            hover
+            variant="light"
+            className="table table-bordered table-striped mb-0"
+          >
+            <thead>
+              <tr>
+                <th className="text-center col col-lg-2">Card Name</th>
+                <th className="text-center col col-lg-2">Card Image</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredCollection.map((row) => {
+                if (row.back_picture_url) {
+                  return (
+                    <tr key={row.multiverse_id}>
+                      <td className="text-center">
+                        {row.name} <br></br>
+                        <p className="mt-3">Quantity: {row.quantity}</p>
+                        <Button
+                          variant="danger"
+                          size="xs"
+                          onClick={() => handleDelete(row.multiverse_id)}
+                        >
+                          Remove one card
+                        </Button>
                         <br></br>
-                          <Link to= {`/card/${row.multiverse_id}`}>
-                          <Button vaiant="success">See card details</Button></Link>
+                        <p className="mt-3">Card Price: {row.card_price}</p>
                       </td>
-                </tr>
-            );
-            }else {
-              return (
-                  <tr key ={row.multiverse_id}>
-                    <td className="text-center">{row.name} <br></br><p className="mt-3">Quantity: {row.quantity}</p>
-                      <Button variant="danger" size="xs" onClick={() => handleDelete(row.multiverse_id)}>Remove one card</Button>
-                      <br></br>
-                        <p className="mt-3">Card Price: {row.card_price}</p></td>
-                    <td className="text-center"><img className="p-1" src={row.picture_url} alt="card_picture" width="170px"/>
-                      <br></br>
-                      <Link to= {`/card/${row.multiverse_id}`}><Button vaiant="success">See card details</Button></Link></td>
-                  </tr>
-              );}
-          })}
-        </tbody>
-      </Table>
+                      <td className="text-center">
+                        <img
+                          className="p-1"
+                          src={row.picture_url}
+                          alt="card_picture"
+                          width="170px"
+                        />
+                        <img
+                          className="p-1"
+                          src={row.back_picture_url}
+                          alt="card_picture"
+                          width="170px"
+                        />
+                        <br></br>
+                        <Link to={`/card/${row.multiverse_id}`}>
+                          <Button vaiant="success">See card details</Button>
+                        </Link>
+                      </td>
+                    </tr>
+                  );
+                } else {
+                  return (
+                    <tr key={row.multiverse_id}>
+                      <td className="text-center">
+                        {row.name} <br></br>
+                        <p className="mt-3">Quantity: {row.quantity}</p>
+                        <Button
+                          variant="danger"
+                          size="xs"
+                          onClick={() => handleDelete(row.multiverse_id)}
+                        >
+                          Remove one card
+                        </Button>
+                        <br></br>
+                        <p className="mt-3">Card Price: {row.card_price}</p>
+                      </td>
+                      <td className="text-center">
+                        <img
+                          className="p-1"
+                          src={row.picture_url}
+                          alt="card_picture"
+                          width="170px"
+                        />
+                        <br></br>
+                        <Link to={`/card/${row.multiverse_id}`}>
+                          <Button vaiant="success">See card details</Button>
+                        </Link>
+                      </td>
+                    </tr>
+                  );
+                }
+              })}
+            </tbody>
+          </Table>
+        </div>
       </div>
-      </div>
-    );}
-
+    );
+  }
 }
