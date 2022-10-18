b6bcd5bd39b8fda200dc12fdf01dfddbb7f47c2f
Sean Kennedy <snkennedy21@gmail.com>
2022-10-18 15:38:44 -0400

    save

--- ghi/src/components/ui/Navbar.js
+++ ghi/src/components/ui/Navbar.js
@@ -45,19 +45,6 @@ function NavScrollExample() {
             <Nav.Link eventKey="/login">Login</Nav.Link>
             <Nav.Link eventKey="/signup">Signup</Nav.Link>
             <Nav.Link eventKey="/collection">My Collection</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
           </Nav>
           <Form onSubmit={queryScryfallHandler} className="d-flex">
             <Form.Control
