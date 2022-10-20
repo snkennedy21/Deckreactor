871367ad789eb4fefebd7e76afacfe42f0c0b38d
Michael <mbsalvo1@gmail.com>
2022-10-20 16:20:58 -0700

    removed account id from routes. will now be included in initial call. adjusted collection components accordingly

--- api/routers/collections.py
+++ api/routers/collections.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from models import CollectionIn, CollectionOut
+from models import CollectionIn, CollectionOut, AccountOut
 from queries.collections import CollectionQueries
 from .authenticator import authenticator
 from typing import List
@@ -8,20 +8,61 @@ import json
 
 router = APIRouter(tags=["collections"])
 
-@router.get('/collections/{account_id}', response_model=CollectionOut)
+# @router.get('/collections/{account_id}', response_model=CollectionOut)
+# async def get_user_collection(
+#     account_id: str,
+#     repo: CollectionQueries = Depends(),
+# ):
+#     collection = repo.get_one(account_id)
+#     return collection
+
+@router.get('/collections/', response_model=CollectionOut)
 async def get_user_collection(
-    account_id: str,
     repo: CollectionQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ):
+    account = AccountOut(**account_data)
+    account_id = account.id
     collection = repo.get_one(account_id)
     return collection
 
-@router.put('/collections/{account_id}/add/{multiverse_id}', response_model=CollectionOut)
+
+# @router.put('/collections/{account_id}/add/{multiverse_id}', response_model=CollectionOut)
+# async def add_card_to_collection(
+#     account_id: str,
+#     multiverse_id: int,
+#     repo: CollectionQueries = Depends(),
+#     # account_data: dict = Depends(authenticator.get_current_account_data),
+# ):
+#     url = f"https://api.scryfall.com/cards/multiverse/{multiverse_id}"
+#     response = requests.get(url)
+#     content = json.loads(response.content)
+
+#     card_dict = {
+#     "name": content.get('name'),
+#     "multiverse_id": content.get('multiverse_ids')[0],
+#     "card_price": content.get('prices')["usd"],
+#     }
+    
+#     if content.get("layout") in [
+#         "modal_dfc", 
+#         "transform",
+#     ]:
+#         card_dict["picture_url"] = content.get("card_faces")[0].get("image_uris").get("normal")
+#         card_dict["back_picture_url"] = content.get("card_faces")[1].get("image_uris").get("normal")
+#     else:
+#         card_dict["picture_url"] = content.get("image_uris").get("normal")
+
+#     collection = repo.add_card_to_collection(card=card_dict, account_id=account_id)
+
+#     return CollectionOut(**collection)
+
+
+@router.put('/collections/add/{multiverse_id}', response_model=CollectionOut)
 async def add_card_to_collection(
-    account_id: str,
     multiverse_id: int,
     repo: CollectionQueries = Depends(),
-    # account_data: dict = Depends(authenticator.get_current_account_data),
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ):
     url = f"https://api.scryfall.com/cards/multiverse/{multiverse_id}"
     response = requests.get(url)
@@ -42,26 +83,31 @@ async def add_card_to_collection(
     else:
         card_dict["picture_url"] = content.get("image_uris").get("normal")
 
+    account = AccountOut(**account_data)
+    account_id = account.id
     collection = repo.add_card_to_collection(card=card_dict, account_id=account_id)
 
     return CollectionOut(**collection)
     
-@router.put('/collections/{account_id}/remove_one/{multiverse_id}', response_model=CollectionOut)
+@router.put('/collections/remove_one/{multiverse_id}', response_model=CollectionOut)
 def remove_one_card_from_collection(
-    account_id: str,
-    multiverse_id: int,
-    repo: CollectionQueries = Depends(),
-    # account_data: dict = Depends(authenticator.get_current_account_data),
-):
-    collection = repo.remove_one_card_copy_from_collection(multiverse_id=multiverse_id, account_id=account_id)
-    return collection
-
-@router.put('/collections/{account_id}/remove_all/{multiverse_id}', response_model=CollectionOut)
-def remove_all_card_copies_from_collection(
-    account_id: str,
     multiverse_id: int,
     repo: CollectionQueries = Depends(),
     account_data: dict = Depends(authenticator.get_current_account_data),
 ):
+    account = AccountOut(**account_data)
+    account_id = account.id
+    collection = repo.remove_one_card_copy_from_collection(multiverse_id=multiverse_id, account_id=account_id)
+    return collection
+
+@router.put('/collections/remove_all/{multiverse_id}', response_model=CollectionOut)
+def remove_all_card_copies_from_collection(
+
+    multiverse_id: int,
+    repo: CollectionQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+):
+    account = AccountOut(**account_data)
+    account_id = account.id
     collection = repo.remove_all_card_copies_from_collection(multiverse_id=multiverse_id, account_id=account_id)
     return collection
