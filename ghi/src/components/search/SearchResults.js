33eca590737d2726774ca611346caac3dc6886bd
Sean Kennedy <snkennedy21@gmail.com>
2022-10-20 18:50:30 -0400

    add dropdown feature for searchbar

--- ghi/src/components/search/SearchResults.js
+++ ghi/src/components/search/SearchResults.js
@@ -3,11 +3,33 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/esm/Button";
+import Dropdown from "react-bootstrap/Dropdown";
 
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useGetCardsQuery } from "../../store/scryfallApi";
 
 function ContainerExample() {
+  const [usersDecks, setUsersDecks] = useState([]);
+
+  useEffect(() => {
+    async function getDecks() {
+      const decksUrl = "http://localhost:8000/decks/";
+      const fetchConfig = {
+        method: "get",
+        credentials: "include",
+      };
+      const decksResponse = await fetch(decksUrl, fetchConfig);
+      if (decksResponse.ok) {
+        const decksData = await decksResponse.json();
+        setUsersDecks(decksData.decks);
+      }
+    }
+    getDecks();
+  }, []);
+
+  console.log(usersDecks);
+
   const search = useSelector((state) => state.search);
   const { data, error, isLoading } = useGetCardsQuery(search);
 
@@ -40,10 +62,25 @@ function ContainerExample() {
               xs="12"
             >
               <Image
-                className="mb-4"
+                className="mb-1"
                 src={card.picture_url}
                 style={{ width: "100%" }}
               />
+              <Dropdown className="mb-4">
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Add To Deck
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item>My Collection</Dropdown.Item>
+                  <Dropdown.Divider />
+                  {usersDecks.map((deck) => {
+                    return (
+                      <Dropdown.Item key={deck.id}>{deck.name}</Dropdown.Item>
+                    );
+                  })}
+                </Dropdown.Menu>
+              </Dropdown>
             </Col>
           );
         })}
