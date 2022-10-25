de7f5cfdf5b02f2202233cc46788b679b134bc03
Devin Wright <dtw2111@gmail.com>
2022-10-25 00:31:12 -0400

    continued to integrate RTK query store into components, and refactored detail page code into separate files

---
+++ ghi/src/components/card-details/ParseSymbolsAndLineBreaks.js
@@ -0,0 +1,64 @@
+import { useGetSymbolsQuery } from "../../store/scryfallWebApi";
+import React from "react";
+
+// wrap any text in this to apply card symbols and line breaks to text string
+
+function ParseSymbolsAndLineBreaks(props) {
+  const string = props.string;
+  const { data: symbols, error: symbolsError, isLoading: symbolsIsLoading } = useGetSymbolsQuery();
+  
+  const symbolUrls = {}
+  if (symbols) {
+    // assemble symbolUrls object {symbol: svg_uri}
+    for (let symbol of symbols.data) {
+      symbolUrls[symbol.symbol] = symbol.svg_uri;
+    }
+  }
+  
+  // helper function for parseSymbolsAndLineBreaks
+  function substringSymbolArray(s) {
+    const output = []
+    let substring = ""
+    for (let i=0; i<s.length; i++) {
+      if (s[i] === "{") {
+        output.push(substring);
+        substring = "";
+        while (i < s.length && s[i] !== "}") {
+          substring += s[i];
+          i++;
+        }
+        substring += s[i];
+        output.push(substring);
+        substring = "";
+      } else {
+        substring += s[i];
+      }
+      if (i === s.length-1) {
+        output.push(substring);
+      }
+    }
+    return output;
+  }
+
+  return (
+    <React.Fragment>
+    {string.split("\n").map((substring, index) => 
+      <span key={`${string} ${substring} ${index}`}>
+      {substringSymbolArray(substring).map((item, idx) => 
+        <React.Fragment key={`${string} ${item} ${idx} fragment`}>
+        {
+        item in symbolUrls ? // replaces substring with corresponding symbol
+        <img style={{height: "1em"}} key={`${string} ${item} ${idx}`} src={symbolUrls[item]} />
+        :
+        <span key={`${string} ${item} ${idx}`}>{item}</span>
+        }
+        </React.Fragment>
+      )}
+      <br/>
+      </span>
+    )}
+    </React.Fragment>
+  )
+}
+
+export default ParseSymbolsAndLineBreaks;
---
+++ ghi/src/components/card-details/getBackground.js
@@ -0,0 +1,56 @@
+const backgrounds = {
+  R: [
+    "https://media.magic.wizards.com/images/wallpaper/wrenn-and-six-2x2-background-2560x1600.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/koll_the_forgemaster_khm_2560x1600_wallpaper_0.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/Blood_Sun_RIX_2560x1600_Wallpaper.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/Chandra_PW_2560x1600_Wallpaper.jpg",
+  ],
+  W: [
+    "https://media.magic.wizards.com/images/wallpaper/CityofBrass_MMA_2560x1600_Wallpaper.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/Azor-the-Lawbringer_RIX_2560x1600_Wallpaper.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/Angelic-Page_A25_2560x1600_Wallpaper.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/Weatherlight_DAR_2560x1600_Wallpaper.jpg",
+  ],
+  B: [
+    "https://media.magic.wizards.com/images/wallpaper/Wallpaper_Erebos_God_ofthe_Dead_2560x1600.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/Campaign-of-Vengeance_EMN_2560x1600_Wallpaper.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/PollutedDelta_2560x1600_Wallpaper.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/callofthenightwing_GTC_2560x1600_Wallpaper.jpg",
+  ],
+  U: [
+    "https://media.magic.wizards.com/images/wallpaper/grand-arbiter-augustin-iv-2x2-background-2560x1600.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/422742_inquisitor_greyfax_2560x1600_wallpaper.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/tidechannel-pathway_khm_2560x1600_wallpaper.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/tidechannel-pathway_khm_2560x1600_wallpaper.jpg",
+  ],
+  G: [
+    "https://media.magic.wizards.com/images/wallpaper/sparas_headquarters_kieran_yanner_2560x1600_wpoozxbqpcw.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/quandrixcommand_stx_2560x1600_wallpaper.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/timbercrown-pathway_sld_2560x1600_wallpaper.jpg",
+    "https://media.magic.wizards.com/images/wallpaper/Tarmogoyf_DGM_2560x1600_Wallpaper.jpg",
+  ],
+}
+
+const colors_to_abbrevs = {
+  "blue": "U",
+  "green": "G",
+  "black": "B",
+  "red": "R",
+  "white": "W",
+}
+
+function getBackground(color) {
+  if (color.toLowerCase() in Object.keys(colors_to_abbrevs)) {
+    color = colors_to_abbrevs[color.toLowerCase()];
+  }
+  if (color in Object.keys(backgrounds)) {
+    return backgrounds[color][Math.floor(Math.random() * backgrounds[color].length)];
+  } else {
+    const allBackgrounds = backgrounds.R.concat(backgrounds.W, backgrounds.B, backgrounds.G, backgrounds.U);
+    return (
+      allBackgrounds[Math.floor(Math.random() * allBackgrounds.length)]
+    )
+  }
+}
+
+export default getBackground;
