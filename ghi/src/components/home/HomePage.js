b960cfa692b7cc620ec8f12e8fcb3a3110b4af5d
Sean Kennedy <snkennedy21@gmail.com>
2022-10-31 17:28:47 -0400

    test

--- ghi/src/components/home/HomePage.js
+++ ghi/src/components/home/HomePage.js
@@ -120,6 +120,7 @@ function HomePage() {
               >
                 Random Card
               </Button>
+              <Button>Hello</Button>
             </div>
           </Container>
         </div>
